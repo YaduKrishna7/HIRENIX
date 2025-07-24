a5ffdfada1f2775475b95934560de4b593e84728
Yadu Krishna <yadukrishna@Yadus-MacBook-Pro.local>
2025-07-24 20:05:03 +0530

    2nd commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,11 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
 from .import views
 from django.contrib.auth import views as auth_views
+from django.shortcuts import redirect
 
 urlpatterns = [
 
 
     # Authentication URLs
+    path('admin/', admin.site.urls),
+    path('', lambda request: redirect('login')),
     path('signup/', views.candidate_signup, name='signup'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
